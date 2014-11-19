23267f0f74a25ed8efb4654b7787070cf1acc405
wzb <bangzewu@126.com>
2014-11-19 17:15:53 +0800

    add wxj swauthkey error, and fix it

--- client/test_user.sh
+++ client/test_user.sh
@@ -3,7 +3,7 @@ if [ $1 = 'add' ]; then
 	swauth-add-account -K swauthkey Android  -G 'java,it,cs,android,developer,coder,designer,game,application,ad,fressman,veteran' -I 'android develop group'
 	swauth-add-user -K swauthkey -a wzb wzb wzb -G 'Android'
 	swauth-add-user -K swauthkey -a Android wzb wzb -I 'java,it,cs,android,fressman,game,developer'
-	swauth-add-user -K swauhtkey -a wxj wxj wxj -G 'Android'
+	swauth-add-user -K swauthkey -a wxj wxj wxj -G 'Android'
 	swauth-add-user -K swauthkey -a Android wxj wxj -I 'java,it,cs,android,fressman,appliaction,designer'
 elif [ $1 = 'remove' ]; then
 	swauth-delete-user -K swauthkey wzb wzb
