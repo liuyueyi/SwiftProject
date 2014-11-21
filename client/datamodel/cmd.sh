de789e28d3cf85527215a6c2f4a909af19736d5d
wzb <bangzewu@126.com>
2014-11-21 17:12:18 +0800

    remove remote item, this shell is used for basic operation

--- client/datamodel/cmd.sh
+++ client/datamodel/cmd.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-if [ $1 = 'remote' ]; then
-	sudo swift -A http://192.168.0.122:8080/auth/v1.0 -U Android:wzb -K wzb $2 $3 $4 $5 $6 $7
-elif [ $1 = 'local' ]; then
-	sudo swift -A http://127.0.0.1:8080/auth/v1.0 -U Android:wzb -K wzb $2 $3 $4 $5 $6 $7
+if [ $1 != 'help' ]; then
+	sudo swift -A http://127.0.0.1:8080/auth/v1.0 -U Android:wzb -K wzb $1 $2 $3 $4 $5 $6 $7
 else
-	echo 'command format: [ remote | local ] + [ post | upload | download | list | stat ] + ...'
+	echo 'command format: [ post | upload | download | list | stat ] + ...'
 fi
